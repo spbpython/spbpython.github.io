8dd6461b9e86f76836e0c916bb6c975139007330
Danil Ivanov <nonamenix@gmail.com>
2016-06-19 14:37:08 +0300

    Initial commit with vue.js application

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>SPB Python Guru</h1>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import 'normalize.css'
+
+export default {
+  components: {
+    Hello
+  }
+}
+
+</script>
+
+<style>
+  html {
+    height: 100%;
+    background: url(./assets/backgrounds/st-petersburg.jpg) 100% 100% no-repeat;
+    background-size: cover;
+  }
+
+  body {
+    font-family: 'Roboto Condensed', sans-serif;
+    height: 100%;
+  }
+</style>
