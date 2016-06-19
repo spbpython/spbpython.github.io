6491ca1e12afa3989b6625c458d91db0458e5389
Danil Ivanov <nonamenix@gmail.com>
2016-06-19 22:59:04 +0300

    First version with fullpage.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,50 @@
 <template>
   <div id="app">
-    <h1>SPB Python Guru</h1>
+    <div id="fullpage">
+
+      <div class="section " id="section-first">
+        <div class="intro">
+          <h1>SPb Python <br>Guru</h1>
+          <img src="./assets/logo.svg" alt="">
+
+          <p>Сообщество Python-программистов Санкт-Петербурга.</p>
+        </div>
+      </div>
+
+      <div class="section" id="section-second">
+        <div class="slide" id="slide1">
+          <div class="intro">
+            <h1>Общаемся в оффлайне</h1>
+            <a href="http://www.meetup.com/spbpython/">
+              <img class="meetup" target="_blank" src="./assets/networks/meetup.png" alt="">
+            </a>
+
+            <p>
+              Расписание встреч на <a target="_blank" href="http://www.meetup.com/spbpython/">Meetup.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="section" id="section-third">
+        <div class="intro">
+          <h1>Общаемся в онлайне</h1>
+          <p>
+            <a href="https://plus.google.com/communities/107328406287050920928">
+              <img class="google-plus" target="_blank" src="./assets/networks/google-plus.png" alt="SPb Python Guru in Google Plus">
+            </a>
+            <a href="https://telegram.me/spbpython">
+              <img class="telegram" target="_blank" src="./assets/networks/telegram.png" alt="SPb Python Guru in Telegram">
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-import 'normalize.css'
-
 export default {
-  components: {
-    Hello
-  }
+  components: {}
 }
-
 </script>
-
-<style>
-  html {
-    height: 100%;
-    background: url(./assets/backgrounds/st-petersburg.jpg) 100% 100% no-repeat;
-    background-size: cover;
-  }
-
-  body {
-    font-family: 'Roboto Condensed', sans-serif;
-    height: 100%;
-  }
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App'
+import $ from 'jquery'
+import 'fullpage.js'
+import 'fullpage.js/jquery.fullPage.css'
+
+import './css/normalize.css'
+
+import './css/app.css'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
+
+})
+
+$(document).ready(function () {
+  $('#fullpage').fullpage({
+    anchors: ['wellcome', 'community', 'links'],
+//    sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['First', 'Second', 'Third']
+  })
 })
